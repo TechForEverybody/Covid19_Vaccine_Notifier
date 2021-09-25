83a3004b7ca51803fd968c6c69f8026c0ed69359
SHIVKUMAR CHAUHAN <Chauvhanshgud19ce@student.mes.ac.in>
2021-09-25 14:08:59 +0530

    notifier music added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let vaccinename = 'COVAXIN'
 let pincode = '410218'
 let vaccinationcenterdata = []
 let starttime
+let notifier_music=new Audio('./Notifier_Music.mp3')
 setInterval(() => {
     timeplace = document.getElementById('time')
     time = Date()
@@ -25,20 +26,22 @@ async function getVaccineNotification() {
         let centerdata = await response.json()
         // console.log(centerdata);
         centerdata.centers.forEach(element => {
+            // console.log(element);
             if (element.fee_type === 'Free') {
                 // console.log(element.sessions);
 
                 for (let i = 0; i < element.sessions.length; i++) {
                     let value = element.sessions[i];
                     // console.log(value);
-                    // console.log(value.available_capacity_dose1);
+                    console.log(value.available_capacity_dose1);
                     if (value.vaccine == vaccinename) {
                         if (value.available_capacity_dose1 > 0) {
-                            // console.log('available');
+                            console.log('available');
+                            notifier_music.play()
                             vaccinationcenterdata.push(element)
                             break
                         } else {
-                            // console.log('not available');
+                            console.log('not available');
                             // console.log(element)
                         }
                     }
@@ -53,7 +56,7 @@ async function getVaccineNotification() {
         // console.log(vaccinationcenterdata.length);
         if (vaccinationcenterdata.length > 0) {
             let vaccinationdataContainerdata = `
-            <h1 style="background:aquamarine;color#fff;width:50%;padding:10px;text-align:center;font-size:30px;">
+            <h1 style="background:aquamarine;color:#fff;width:50%;padding:10px;text-align:center;font-size:30px;">
                     Vaccine Is Available
                 </h1>
             <div id="listofcenters">
@@ -61,24 +64,24 @@ async function getVaccineNotification() {
             vaccinationcenterdata.forEach((value, index) => {
                 // console.log(value);
                 vaccinationdataContainerdata = vaccinationdataContainerdata + `
-                <div class="vaccinationplacecontainer"> 
-                <h1>${value.fee_type}</h1>
-                <h4>Center ID :  ${value.center_id} </h4>
-                <h4>Center Name : ${value.name} </h4>
-                <h4>Address : ${value.address} , ${value.block_name} , ${value.district_name} , ${value.state_name} </h4>
-                <h4>Time Slot : ${value.from} to ${value.to}</h4>
+                <div class="vaccinationplacecontainer">
+                    <h1>${value.fee_type}</h1>
+                    <h4>Center ID : ${value.center_id} </h4>
+                    <h4>Center Name : ${value.name} </h4>
+                    <h4>Address : ${value.address} , ${value.block_name} , ${value.district_name} , ${value.state_name} </h4>
+                    <h4>Time Slot : ${value.from} to ${value.to}</h4>
                 `
                 value.sessions.forEach((value, index) => {
                     // console.log(value);
                     vaccinationdataContainerdata = vaccinationdataContainerdata + `
                     <div class="vaccineplace">
-                    <h3>${value.vaccine}</h3>
-                    <h5>Date :  ${value.date} </h5>
-                    <h5>Minimun Age Range : ${value.min_age_limit} </h5>
-                    <div>
-                    <span>Dose-1 : <span class="desecount"> ${value.available_capacity_dose1}</span> </span>
-                    <span>Dose-2 : <span class="desecount"> ${value.available_capacity_dose2}</span> </span>
-                    </div>
+                        <h3>${value.vaccine}</h3>
+                        <h5>Date : ${value.date} </h5>
+                        <h5>Minimun Age Range : ${value.min_age_limit} </h5>
+                        <div>
+                            <span>Dose-1 : <span class="desecount"> ${value.available_capacity_dose1}</span> </span>
+                            <span>Dose-2 : <span class="desecount"> ${value.available_capacity_dose2}</span> </span>
+                        </div>
                     </div>
                     `
                 })
@@ -88,6 +91,7 @@ async function getVaccineNotification() {
             alert_container.innerHTML = vaccinationdataContainerdata + `</div>`
         } else {
             // getVaccineNotification()
+            notifier_music.pause()
             alert_container.innerHTML = `
             <div class="notavailable">
             <h2>
@@ -112,8 +116,12 @@ getVaccineNotification()
 async function updatepincode(event) {
     // console.log(event);
     // console.log(event.target.value);
-    pincode = event.target.value
-    await getVaccineNotification()
+    if (event.target.value.length===6) {
+        pincode = event.target.value
+        await getVaccineNotification()
+    } else {
+        window.alert('भाई प्लीज 6 डिजिट का पिनकोड इंटर कर ना')
+    }
 }
 
 async function updatevaccinename(event) {
